079c5c854128466037797677df650be2a13185cf
nidhi <nidhi.sengar@mail.vinove.com>
2018-04-06 17:40:53 +0530

    chain blast

--- src/box/box.js
+++ src/box/box.js
@@ -6,10 +6,10 @@ class BOX extends Component{
         
         let maxBalls = 0;
         let { rowNumber, colNumber } = this.props;
-        if((rowNumber === 0 && colNumber === 0) || (rowNumber === 0 && colNumber === 5) || (rowNumber == 5 && colNumber == 0) || (rowNumber === 5 && colNumber === 5)){
+        if((rowNumber === 0 && colNumber === 0) || (rowNumber === 0 && colNumber === 5) || (rowNumber === 8 && colNumber === 0) || (rowNumber === 8 && colNumber === 5)){
             maxBalls = 1
         }
-        else if((((rowNumber === 0 || rowNumber === 5) &&  (0 < colNumber < 5))) || ((colNumber === 0 || colNumber === 5) && (0< rowNumber < 5))){
+        else if((((rowNumber === 0 || rowNumber === 8) &&  (0 < colNumber < 5))) || ((colNumber === 0 || colNumber === 5) && (0< rowNumber < 8))){
             maxBalls = 2
         }
         else{
@@ -17,23 +17,93 @@ class BOX extends Component{
         }
         this.state = {
             maxBalls,
-            currentBall: 0
+            currentBall: 0,
+            isNext: false
         }
         this.insertBall = this.insertBall.bind(this);
         this.ballArr = [];
     }
 
     insertBall() {
+        console.log(".....insertBall")
         if(this.state.maxBalls > this.state.currentBall){
+            this.setState({    
+                currentBall: this.state.currentBall +1,
+                isNext: !this.state.isNext
+            })
+        }else{
+            let {rowNumber, colNumber} = this.props;
+            let array;
+            if(this.state.maxBalls === 1){
+                if( rowNumber === 0 && colNumber === 0){
+                    array  = [{row: rowNumber, col: colNumber+1},{row: rowNumber+1,col: colNumber}];  
+                }else if(rowNumber === 0 && colNumber === 5){
+                    array  = [{row: rowNumber, col: colNumber-1},{row: rowNumber+1,col: colNumber}];
+                }else if(rowNumber === 8 && colNumber === 0){
+                    array  = [{row: rowNumber-1, col: colNumber},{row: rowNumber,col: colNumber+1}];
+                }else{
+                    array  = [{row: rowNumber-1, col: colNumber},{row: rowNumber,col: colNumber-1}];
+                }
+            }
+            if(this.state.maxBalls === 2){
+                if(rowNumber === 0 ){
+                    array = [{row: rowNumber, col: colNumber-1},{row: rowNumber, col: colNumber+1},{row: rowNumber+1, col: colNumber}]
+                }
+                if(rowNumber === 8 ){
+                    array = [{row: rowNumber, col: colNumber-1},{row: rowNumber, col: colNumber+1},{row: rowNumber-1, col: colNumber}]
+                }
+                if(colNumber === 0 ){
+                    array = [{row: rowNumber-1, col: colNumber},{row: rowNumber+1, col: colNumber},{row: rowNumber, col: colNumber+1}]
+                }
+                if(colNumber === 5 ){
+                    array = [{row: rowNumber-1, col: colNumber},{row: rowNumber+1, col: colNumber},{row: rowNumber, col: colNumber-1}]
+                }
+            }
+            if(this.state.maxBalls === 3){
+                array = [{row: rowNumber-1, col: colNumber},{row: rowNumber+1, col: colNumber},{row: rowNumber, col: colNumber-1},{row: rowNumber, col: colNumber+1}];
+            }
             this.setState({
-                currentBall: this.state.currentBall +1
+                currentBall: 0,
+            }, () => {
+                this.props.callback(array);
             })
         }
     }
 
     render(){
+       let ball = !this.state.currentBall ? null : this.state.currentBall;
+       let turn = this.state.isNext;
+       console.log("...turn",turn);
+    
         console.log("....this.props.value",this.props.value);
-        return <button className="boxes" onClick={this.insertBall}>{!this.state.currentBall ? null : this.state.currentBall }</button>
+        return (
+        
+        <button className="boxes" onClick={this.insertBall}>
+        {
+            ball == 1 &&  <div className="dots"></div>
+        }{
+            ball == 2 && (
+            <div>
+                <div className="dots"></div>
+                <div className="dots second"></div>
+            </div>
+            )
+        }
+        {
+            ball == 3 && (
+            <div> 
+                <div className="dots"></div>
+                <div className="dots second"></div>
+                <div className="dots third"></div>
+            </div>
+            )
+        }
+       
+        </button>
+        
+         )
     }
 }
 export default BOX;
+
+// {this.props.rowNumber}{this.props.colNumber}
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -3,25 +3,48 @@ import BOX from '../box/box';
 class Layout extends Component{
     constructor(props){
         super(props);
-        this.state ={rows: Array(6).fill(null), cols: Array(6).fill(null)};
+        this.state ={rows: Array(9).fill(null), cols: Array(6).fill(null),noOfUsers: 2};
+        this.setBallValueFromChild = this.setBallValueFromChild.bind(this);
+        this.selectUser = this.selectUser.bind(this);
     }
     render(){
         return(
-            <div>
+            <div className="col-md-8 col-md-offset-2">
+                <select className="form-control" ref="noOfUsers" onChange={this.selectUser}>
+                    <option value="2">2 Users</option>
+                    <option value="3">3 Users</option>
+                    <option value="2">4 Users</option>
+                    <option value="2">5 Users</option>
+                    <option value="2">6 Users</option>
+                </select>
+            <div className="layout container-fluid">
             {
                 this.state.rows.map((item,row)=>(
                     <div key={row} className="col-md-6 col-md-offset-4 box-container">
                     {
                         this.state.cols.map((item,index) => (
-                            <BOX key={row+index} rowNumber={row} colNumber={index}/>
+                            <BOX key={row+index} rowNumber={row} ref={`${row}${index}`} noOfUsers={this.state.noOfUsers} colNumber={index} callback={this.setBallValueFromChild}/>
                         ))
                     }
                     </div>
                 ))
             }
             </div>
+            </div>
            
         )
+    }
+    setBallValueFromChild(array){
+        console.log("....row number",array);
+        array.forEach(item => {
+            setTimeout(()=> {
+                this.refs[`${item.row}${item.col}`].insertBall();
+            }, 200)
+        });
+    }
+    selectUser(e){
+        let noOfUser = this.refs.noOfUsers.value;
+        console.log("....no of users", noOfUser); 
     }
    
 }
